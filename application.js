9c62c927cdc25c328002e279d8b6e4766d13ef17
Wes <wesley.ch.yu@gmail.com>
2015-07-31 17:47:15 -0700

    search displays weather and city

--- application.js
+++ application.js
@@ -3,20 +3,21 @@ $(function () {
 
   var $search = $("#search");
   var $results = $("#results");
+  var weatherDisplay = $('#weather-display').fadeTo(0, 0);
 
   var searchCall = function(query) {
     $.ajax ({
       url: "http://autocomplete.wunderground.com/aq?query=" + query + "&cb=?",
       type: "get",
       dataType: "jsonp",
-      success: function(data){
+      success: function(data)
+      {
         mydata = data.RESULTS
         var results = $("<ul>").attr("id", "results-field")
         for (var i = 0; i < 7; i++) {
           $("<li>").text(data.RESULTS[i].name).addClass("location").appendTo(results);
         }
-        $($results).empty();
-        $(results).appendTo($results);
+        $results.empty().append(results);
       },
     })
   };
@@ -26,11 +27,16 @@ $(function () {
       url : "http://api.wunderground.com/api/afb0f7d8bcdda471/geolookup/conditions/q/IA/" + query + ".json",
       dataType : "jsonp",
       success : function(data) {
-        stuff = data
         if (data.current_observation != undefined) {
-          var location = data['location']['city'];
-          var temp_f = data['current_observation']['temp_f'];
-          alert("Current temperature in " + location + " is: " + temp_f);
+          var location = data.location.city;
+          var temp_f = data.current_observation.temp_f;
+          $("#location").text(location)
+          $("#temp").text(temp_f + "°f")
+          $("#search-box").fadeOut(500, function() {
+            $('.body').hide();
+            weatherDisplay.fadeTo(500, 1);
+          })
+
         } else {
             var location = data.response.results[0]["city"] + ", " + data.response.results[0]["country_name"];
             alert("There's no weather data for " + location);
@@ -38,6 +44,13 @@ $(function () {
       }
     });
   };
+
+  $("#back-button").on("click", function(){
+    $("#weather-display").fadeOut(500, function(){
+      $(".body").show();
+      $("#search-box").show().fadeTo(500, 1);
+    })
+  })
 
   $(document).on("click", ".location", function(){
     var location = $(this).text().split(",")[0].replace(" ", "_");
